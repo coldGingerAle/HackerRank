fe36df9debf01d5ce54bb7601b25ada1cd362dc7
coldGingerAle <Hui.Lin69@qmail.cuny.edu>
2017-03-06 19:34:09 -0500

    strange advertising

---
+++ Algorithms/Implementation/strangeAdvertising.cpp
@@ -0,0 +1,26 @@
+/*
+	The problem can be found at 
+	https://www.hackerrank.com/challenges/strange-advertising
+*/
+
+#include <cmath>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int Likes(int n) {
+    if (n == 1) return 2;
+    return floor((3 * Likes(n - 1) / 2));
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += floor(Likes(i));
+    }
+    cout << sum;
+    return 0;
+}
+
